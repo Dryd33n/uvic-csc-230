5188ae4519ab0d5bcc0a065df93606b385c29820
Dryd3n <31601254+Dryd33n@users.noreply.github.com>
2025-03-06 02:49:59 -0800

    Some documentation for part a

--- assignments/assignment 3/assignment 3/display_partA.asm
+++ assignments/assignment 3/assignment 3/display_partA.asm
@@ -2,7 +2,9 @@
 .include "lcd.asm"
 
 .cseg
-
+;                         ╔════════════════════════════════════╗                         ;
+;                         ║           INITIALIZATION           ║                         ;                                                                            
+;                         ╚════════════════════════════════════╝                         ;
     ; <-- TIMER DEFINITIONS FOR 1S DELAY -->
     .equ CLOCK     = 16000000
     .equ PRESCALER = 1024
@@ -11,26 +13,33 @@
 
     ; <-- INITIALIZE STACK POINTER -->
     ldi r16, low(RAMEND)   ; Get RAMEND Low
-	out SPL, r16           ; Initialize Stack Pointer Low
-	ldi r16, high(RAMEND)  ; Get RAMEND High
-	out SPH, r16           ; Initialize Stack Pointer High
+    out SPL, r16           ; Initialize Stack Pointer Low
+    ldi r16, high(RAMEND)  ; Get RAMEND High
+    out SPH, r16           ; Initialize Stack Pointer High
 
     ; <-- INITIALIZE TIMER -->
     rcall timer_init
 
     call lcd_init			; call lcd_init to Initialize the LCD
-	call init_strings
+    call init_strings                   ; load strings into data memory
 
+
+;                         ╔════════════════════════════════════╗                         ;
+;                         ║        MAIN EXECUTION LOOP         ║                         ;                                                                            
+;                         ╚════════════════════════════════════╝                         ;
     ; <-- ENTER MAIN LOOP -->
     loop:
-	    rcall display_line1
+	; -- DISPLAY BOTH LINES --
+	rcall display_line1
         rcall display_line2
         rcall delay_1s
 
+	; -- DISPLAY FIRST LINE --
         call lcd_clr
         rcall display_line1
         rcall delay_1s
 
+	; -- DISPLAY SECOND LINE
         call lcd_clr
         rcall display_line2
         rcall delay_1s
@@ -38,26 +47,87 @@
         rjmp loop
 
 
+
+
+
+
+
+
+
+
+;========================================================================================;
+;                         ╔════════════════════════════════════╗                         ;
+;                         ║          INTIAILIZE TIMER          ║                         ;                                                                            
+;                         ╚════════════════════════════════════╝                         ;
+;========================================================================================;
+;                                                                                ;
+;    DESCRIPTION:                                                                ;
+;            - Sets a top value and configures the display mode for timer3       ;
+;    PARAMETERS:                                                                 ;
+;            - TOP a definition containing the top value for the timer           ;
+;                                                                                ;        
+;    OUTPUT:                                                                     ;
+;            - OCR3AH / OCR3AL Holds the top value for timer3                    ;    
+;            - TCCR3A Holds the configuration of PWM mode for timer              ;
+;                                                                                ;
+;    REGISTERS:                                                                  ;
+;            - r16,r17 Used as temporary variables to store timer configuartion  ;
+;                                                                                ;
+;================================================================================;
     timer_init:
         ; <-- SET TOP VALUE -->
-        ldi r16,     high(TOP)
-        ldi r17,     low(TOP)
-        sts OCR3AH,  r16
-        sts OCR3AL,  r17
+        ldi r16,     high(TOP)        ; Load High value of TOP
+        ldi r17,     low(TOP)         ; Load Low value of TOP
+        sts OCR3AH,  r16              ; Set High value of TOP
+        sts OCR3AL,  r17              ; Set Low value of TOP
         
         ; <-- DISABLE PWM MODE -->
-        ldi temp, 0
-        sts TCCR3A, temp
-    ret    
+        ldi r16, 0                    ; Load 0 as configuration for PWM 
+        sts TCCR3A, temp              ; Set 0 as configuration for PWM
+    ret   
+;========================================================================================;
+;                         ╔════════════════════════════════════╗                         ;
+;                         ║        END INTIAILIZE TIMER        ║                         ;                                                                            
+;                         ╚════════════════════════════════════╝                         ;
+;========================================================================================;
 
+
+
+
+
+
+
+
+
+
+;========================================================================================;
+;                         ╔════════════════════════════════════╗                         ;
+;                         ║            DELAY FOR 1S            ║                         ;                                                                            
+;                         ╚════════════════════════════════════╝                         ;
+;========================================================================================;
+;                                                                                ;
+;    DESCRIPTION:                                                                ;
+;            - Halt program execution for 1 second                               ;
+;    PARAMETERS:                                                                 ;
+;            - Assumes OCR3A is set with proper top value for delay              ;
+             - Assumes TCCR3A is set with proper configuration for CTC mode      ;
+;                                                                                ;        
+;    OUTPUT:                                                                     ;
+;            - TCCR3B Holds the configuration for CTC mode and prescaler         ;    
+;            - TIFR3  Reset after timer is done                                  ;
+;                                                                                ;
+;    REGISTERS:                                                                  ;
+;            - r16 Used as temporary variable to store timer configuartion       ;
+;                                                                                ;
+;================================================================================;
     delay_1s:
         ; <-- SET CTC MODE AND PRESCALER -->
-        ldi temp, (1 << WGM32) | (1 << CS32) | (1 << CS30)   ; CTC mode and prescaler 1024
-        sts TCCR3B, temp                                     ; Set CTC and Prescaler
+        ldi r16, (1 << WGM32) | (1 << CS32) | (1 << CS30)   ; CTC mode and prescaler 1024
+        sts TCCR3B, r16                                     ; Set CTC and Prescaler
 
         ; [ BEGIN POLLING TIMER ]
         wait_for_timer:
-            sbic TIFR3, OCF3A      ; Skip if OCF3A (Output Compare Flag) is not set
+            sbic TIFR3, OCF3A       ; Skip if OCF3A (Output Compare Flag) is not set
             rjmp timer_done         ; Jump if OCF3A is set
 
             rjmp wait_for_timer     ; Keep waiting for the timer to overflow
@@ -67,6 +137,11 @@
             sbi TIFR3, OCF3A        ; Clear OCF3A by writing a 1 to it
 
             ret                     ; Return from the subroutine
+;========================================================================================;
+;                         ╔════════════════════════════════════╗                         ;
+;                         ║          END DELAY FOR 1S          ║                         ;                                                                            
+;                         ╚════════════════════════════════════╝                         ;
+;========================================================================================;
 
 
 
@@ -86,6 +161,55 @@
 
 
 
+
+
+
+display_line1:
+    push r16
+
+
+	ldi r16, 0x00
+	push r16
+	ldi r16, 0x00
+	push r16
+	call lcd_gotoxy
+	pop r16
+	pop r16
+
+	; Now display msg1 on the first line
+	ldi r16, high(msg1)
+	push r16
+	ldi r16, low(msg1)
+	push r16
+	call lcd_puts
+	pop r16
+	pop r16
+
+    pop r16
+	ret
+
+display_line2:
+    push r16
+
+    	ldi r16, 0x01
+	push r16
+	ldi r16, 0x00
+	push r16
+	call lcd_gotoxy
+	pop r16
+	pop r16
+
+	; Now display msg2 on the second line
+	ldi r16, high(msg2)
+	push r16
+	ldi r16, low(msg2)
+	push r16
+	call lcd_puts
+	pop r16
+	pop r16
+
+    pop r16
+	ret
 
 init_strings:
 	push r16
@@ -119,53 +243,6 @@ init_strings:
 	pop r16
 
 	pop r16
-	ret
-
-display_line1:
-    push r16
-
-
-	ldi r16, 0x00
-	push r16
-	ldi r16, 0x00
-	push r16
-	call lcd_gotoxy
-	pop r16
-	pop r16
-
-	; Now display msg1 on the first line
-	ldi r16, high(msg1)
-	push r16
-	ldi r16, low(msg1)
-	push r16
-	call lcd_puts
-	pop r16
-	pop r16
-
-    pop r16
-	ret
-
-display_line2:
-    push r16
-
-    ldi r16, 0x01
-	push r16
-	ldi r16, 0x00
-	push r16
-	call lcd_gotoxy
-	pop r16
-	pop r16
-
-	; Now display msg2 on the second line
-	ldi r16, high(msg2)
-	push r16
-	ldi r16, low(msg2)
-	push r16
-	call lcd_puts
-	pop r16
-	pop r16
-
-    pop r16
 	ret
 
 msg1_p:	.db "Dryden Bryson", 0	
